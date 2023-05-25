baad6448970815afb66b87aae861358bd724a654
Roshan Rajapakse <rosh474@gmail.com>
2023-05-25 13:53:50 +0930

    Update GhactionExampleApplicationTests.java

--- src/test/java/nl/brianvermeer/snyk/ghactionexample/GhactionExampleApplicationTests.java
+++ src/test/java/nl/brianvermeer/snyk/ghactionexample/GhactionExampleApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 class GhactionExampleApplicationTests {
 
     @Test
-    void contextLoads() { //test
+    void contextLoads() { //test //test
     }
 
 }
